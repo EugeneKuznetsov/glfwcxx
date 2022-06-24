d2a217e29e14963053c2c77e90e858e50c6d743e
Eugene Kuznetsov <eugene.kuznetsov.de@gmail.com>
2022-06-24 17:36:00 +0200

    feature: setup conan with CMake
    related to #1

---
+++ cmake/Packages.cmake
@@ -0,0 +1,3 @@
+find_package(OpenGL REQUIRED)
+find_package(glfw3 REQUIRED)
+find_package(GTest REQUIRED)
---
+++ conanfile.py
@@ -0,0 +1,47 @@
+import re
+
+from conans import ConanFile, CMake, tools
+from pathlib import Path
+
+required_conan_version = ">=1.45.0"
+
+class GLFWCXX(ConanFile):
+    name = "glfwcxx"
+    author = "Eugene Kuznetsov"
+    license = "MIT"
+    description = "C++17 wrapper of GLFW"
+    url = "https://github.com/EugeneKuznetsov/glfwcxx"
+    scm = {"type": "git", "url": "auto", "revision": "auto"}
+    generators = "cmake_find_package"
+    settings = "os", "compiler", "arch", "build_type"
+    tool_requires = "cmake/[>3.20.x]", "ninja/[^1.11.x]", "gtest/[~1.11.x]"
+    requires = "opengl/system", "glfw/3.3.7"
+
+    def __init__(self, output, runner, display_name="", user=None, channel=None):
+        super().__init__(output, runner, display_name=display_name, user=user, channel=channel)
+        self.version = self.read_version()
+
+    def read_version(self):
+        cmakelists_txt = Path(self.recipe_folder) / "CMakeLists.txt"
+        if cmakelists_txt.exists():
+            content = tools.load(cmakelists_txt)
+            version_regex = "project[\S\s]*VERSION[\S\s](\d+\.\d+\.\d+)"
+            return re.findall(version_regex, content, re.MULTILINE)[0]
+        return None
+
+    def build(self):
+        if self.should_build is True:
+            self.cmake.build()
+        if self.should_test is True:
+            self.cmake.test(output_on_failure=True)
+
+    @property
+    def cmake(self):
+        if not hasattr(self, "_cmake"):
+            cmake_modules = [self.source_folder + "/cmake", self.build_folder]
+            self._cmake = CMake(self, generator="Ninja")
+            self._cmake.definitions["CMAKE_MODULE_PATH"] = ";".join([f.replace("\\", "/") for f in cmake_modules])
+            self._cmake.definitions["CMAKE_EXPORT_COMPILE_COMMANDS"] = True
+            if self.should_configure is True:
+                self._cmake.configure()
+        return self._cmake
